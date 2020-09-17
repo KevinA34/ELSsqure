1c8753811e9abae804c31d2595a1b28f78219420
wangwang <kevin34@foxmail.com>
2020-09-17 23:05:18 +0800

    listView 优化完善可用

--- assets/Script/ListView/ListViewD.js
+++ assets/Script/ListView/ListViewD.js
@@ -1,13 +1,25 @@
+const List = require('List');
 cc.Class({
     extends: cc.Component,
 
+    
     properties: {
-        
+        listV: List,
+
 
     },
 
     onLoad: function () {
 
+        var data = [];
+        for (var i=0; i<100; i++) {
+            data.push(i);
+        }
+        this.listV.numItems = data.length;
+    },
+
+    onListVRender: function(item, _data) {
+        item.listItem.title.string = JSON.stringify(_data);
     },
 
     onDestroy: function() {
